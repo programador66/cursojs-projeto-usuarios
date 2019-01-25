a9e644eadf7521160a86f3ba131b0c7c55c99c16
Caio Cesar <kaio.cesr@gmail.com>
2019-01-25 16:29:06 -0400

    atualizando projeto com MVC aula42

---
+++ controllers/UserController.js
@@ -0,0 +1,125 @@
+class UserController {
+
+    constructor(formId, tableId) {
+
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
+
+    }
+
+    onSubmit() {
+
+        this.formEl.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            let btn = this.formEl.querySelector("[type=submit]");
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            this.getPhoto().then((content) => {
+
+                values.photo = content;
+                this.addLine(values);
+
+                this.formEl.reset();
+                btn.disabled = false;
+
+            }, (e) => {
+                console.error(e);
+
+            });
+
+        });
+
+    }//fechando função onsubmit
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+                reject(e);
+            };
+
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
+
+        });
+
+
+
+
+    }//fechando getphoto
+
+    getValues() {
+
+        let user = {};
+
+        [...this.formEl.elements].forEach(function (field, index) {
+
+
+            if (field.name == "gender") {
+                if (field.checked) {
+                    user[field.name] = field.value;
+                }
+            } else if (field.name == "admin") {
+                user[field.name] = field.checked;
+            } else {
+
+                user[field.name] = field.value;
+            }
+
+        });
+
+        var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+        return objectUser;
+    }//fechando função getValues
+
+
+    addLine(dataUser) {
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = (`
+        <tr>
+            <td><img src="${dataUser.photo}" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'sim' : 'não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+       </tr>
+        `);
+
+        this.tableEl.appendChild(tr);
+
+
+
+    }//fechando função addline
+
+}
